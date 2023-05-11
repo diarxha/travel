bc288729d0b9e2ab9ca4b7c37dffbbce2d4987b5
Diar <diarxharavina@gmail.com>
2023-05-11 21:25:17 +0200

    search bar

--- client/src/Components/Hero.js
+++ client/src/Components/Hero.js
@@ -7,8 +7,14 @@ function Hero() {
     return (
       <div className='navbar'>
         <img src={mbasja} alt="bg" id='mbasja'/>
+        <p className='search-title'>Search Flights</p>
         <div className="flight-search">
-            
+            <div className="search-cities">
+                <p className='cities'>Prishtina</p>
+                <p className='to'><i class="fa-solid fa-arrow-right"></i></p>
+                <p className='cities'>Peja</p>
+            </div>
+            <a href='#' className='search'>Search</a>
         </div>
     </div>
     )
