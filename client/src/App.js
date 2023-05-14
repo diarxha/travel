badde119f5a339cbc16cb0f9073a674a7e7716f7
Diar <diarxharavina@gmail.com>
2023-05-14 14:56:14 +0200

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import Offers from './Components/Offers';
+import Footer from './Components/Footer';
 
 function App() {
   const [users, setUsers] = useState([]); 
@@ -27,6 +28,7 @@ useEffect(() => {
       <Navbar/>
       <Hero/>
       <Offers/>
+      <Footer/>
     </React.Fragment>
     </div>
   );
---
+++ client/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+
+    </div>
+  )
+}
+
+export default Footer
