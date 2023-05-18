7a441fe99e3dd52fdac640f3101659c5d66d0f04
Diar <diarxharavina@gmail.com>
2023-05-18 17:55:00 +0200

    footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -5,7 +5,9 @@ import './Footer.css'
 function Footer() {
   return (
     <div className='footer'>
-
+        <i class="fa-brands fa-instagram" id='insta'></i>
+        <i class="fa-brands fa-twitter" id='twitter'></i>
+        <i class="fa-brands fa-facebook-f" id='facebook'></i>
     </div>
   )
 }
